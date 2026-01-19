047cd8b1110c7fef65f5358d6676408a41b9584a
josh <hey@imjosh.dev>
2026-01-19 14:32:23 -0600

    fix: x init pos

--- blocks.go
+++ blocks.go
@@ -1,13 +1,13 @@
 package tetris
 
 var (
-	t  = Block{x: 2, y: 2, color: 2, shape: [][]bool{{false, true, false}, {true, true, true}}}
-	l  = Block{x: 2, y: 2, color: 3, shape: [][]bool{{false, false, false, false}, {true, true, true, true}, {false, false, false, false}, {false, false, false, false}}}
-	j  = Block{x: 2, y: 2, color: 4, shape: [][]bool{{true, false, false}, {true, true, true}, {false, false, false}}}
-	j2 = Block{x: 2, y: 2, color: 5, shape: [][]bool{{false, false, true}, {true, true, true}, {false, false, false}}}
-	o  = Block{x: 2, y: 2, color: 6, shape: [][]bool{{true, true}, {true, true}}}
-	z  = Block{x: 2, y: 2, color: 7, shape: [][]bool{{false, true, true}, {true, true, false}, {false, false, false}}}
-	z2 = Block{x: 2, y: 2, color: 8, shape: [][]bool{{true, true, false}, {false, true, true}, {false, false, false}}}
+	t  = Block{x: 4, y: 2, color: 2, shape: [][]bool{{false, true, false}, {true, true, true}}}
+	l  = Block{x: 4, y: 2, color: 3, shape: [][]bool{{false, false, false, false}, {true, true, true, true}, {false, false, false, false}, {false, false, false, false}}}
+	j  = Block{x: 4, y: 2, color: 4, shape: [][]bool{{true, false, false}, {true, true, true}, {false, false, false}}}
+	j2 = Block{x: 4, y: 2, color: 5, shape: [][]bool{{false, false, true}, {true, true, true}, {false, false, false}}}
+	o  = Block{x: 4, y: 2, color: 6, shape: [][]bool{{true, true}, {true, true}}}
+	z  = Block{x: 4, y: 2, color: 7, shape: [][]bool{{false, true, true}, {true, true, false}, {false, false, false}}}
+	z2 = Block{x: 4, y: 2, color: 8, shape: [][]bool{{true, true, false}, {false, true, true}, {false, false, false}}}
 )
 
 var Blocks = []Block{t, l, j, j2, o, z, z2}
